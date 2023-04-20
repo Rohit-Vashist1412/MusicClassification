1f814715c580f8cefdf340052705522d55b86d26
rohitvashist1412 <rohitvashist1412@gmail.com>
2023-04-20 22:27:10 +0100

    implement get songs func need to add more stats

--- SpotifyGetSongs.py
+++ SpotifyGetSongs.py
@@ -3,9 +3,24 @@ from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+
 load_dotenv()
 
-client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv("sp_client_id"), 
-client_secret=os.getenv("sp_client_secret"))
-sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+def get_tracks(URI):
+# Get URI in proper format for API
+    URI = URI.split(',')[-1].split('?')[0]
+    
+    songs = {'artistName':[],'songName':[]}
+    
+    #Auth uses env vars
+    client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv("sp_client_id"), 
+    client_secret=os.getenv("sp_client_secret"))
+    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+    items = sp.playlist_tracks('37i9dQZF1DXcBWIGoYBM5M')
 
+    for item in items['items']:
+        artistName = [artist['name'] for artist in item['track']['album']['artists']]
+        songName = item['track']['name']
+        songs['artistName'].append(artistName)
+        songs['songName'].append(songName)
+    return songs
