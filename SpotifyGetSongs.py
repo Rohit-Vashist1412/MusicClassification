65314297388ac486da7a6ec391b0f2dcd569df2b
Rohit-Vashist1412 <100518595+Rohit-Vashist1412@users.noreply.github.com>
2023-05-17 10:45:06 +0100

    Refactor as OOP code

--- SpotifyGetSongs.py
+++ SpotifyGetSongs.py
@@ -1,58 +1,53 @@
 import os
-from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-load_dotenv()
-#Auth uses env vars
-
-client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv("sp_client_id"), 
-client_secret=os.getenv("sp_client_secret"))
-sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-
-def get_playlist_tracks(URI):
-# Get URI in proper format for API
-    URI = URI.split('/')[-1].split('?')[0].split(':')[-1]
-    
-    songs_dict = {'track_id':[],'artist_name':[],'song_name':[]}
-    
-    items = sp.playlist_tracks(URI)
-
-    for item in items['items']:
-        artist_name = [artist['name'] for artist in item['track']['album']['artists']]
-        song_name = item['track']['name']
-        track_id = item['track']['id']
-        songs_dict['artist_name'].append(artist_name)
-        songs_dict['song_name'].append(song_name)
-        songs_dict['track_id'].append(track_id)
-    return songs_dict
+class SpotifyAPI:
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.client_credentials_manager = SpotifyClientCredentials(client_id=self.client_id, 
+                                                                   client_secret=self.client_secret)
+        self.sp = spotipy.Spotify(client_credentials_manager=self.client_credentials_manager)
 
 
-def pull_new_releases():
-    URI_list = []
-    albums = sp.new_releases()
-    for album in albums['albums']['items']:
-         URI_list.append(album['uri'].split(':')[-1])
-         '''should add something to write new release ids to a file so
-         when checking daily it only pulls data not already pulled''' 
+    def get_playlist_tracks(self, URI):
+        URI = URI.split('/')[-1].split('?')[0].split(':')[-1]
+        songs_dict = {'track_id':[], 'artist_name':[], 'song_name':[]}
+        items = self.sp.playlist_tracks(URI)
 
-    return URI_list 
-
-##Albums and playlist data are stored differently so write an alternate func to pull album data 
-
-def pull_album_tracks(URI_list):
-
-    songs_dict = {'track_id':[],'artist_name':[],'song_name':[]}
-    URI_list = [URI.split('/')[-1].split('?')[0].split(':')[-1] for URI in URI_list]
-    items = sp.albums(URI_list)
-    
-    for item in items['albums']:
-        for track in item['tracks']['items']:
-            song_name = track['name']
-            track_id = track['id']
-            artist_name = [name['name'] for name in track['artists']] 
-            songs_dict['track_id'].append(track_id)
-            songs_dict['song_name'].append(song_name)
+        for item in items['items']:
+            artist_name = [artist['name'] for artist in item['track']['album']['artists']]
+            song_name = item['track']['name']
+            track_id = item['track']['id']
             songs_dict['artist_name'].append(artist_name)
-    return songs_dict
+            songs_dict['song_name'].append(song_name)
+            songs_dict['track_id'].append(track_id)
+        
+        return songs_dict
 
+    def pull_new_releases(self):
+        URI_list = []
+        albums = self.sp.new_releases()
+        for album in albums['albums']['items']:
+            URI_list.append(album['uri'].split(':')[-1])
+            '''should add something to write new release ids to a file so
+            when checking daily it only pulls data not already pulled'''
+        
+        return URI_list 
+
+    def pull_album_tracks(self, URI_list):
+        songs_dict = {'track_id':[], 'artist_name':[], 'song_name':[]}
+        URI_list = [URI.split('/')[-1].split('?')[0].split(':')[-1] for URI in URI_list]
+        items = self.sp.albums(URI_list)
+        
+        for item in items['albums']:
+            for track in item['tracks']['items']:
+                song_name = track['name']
+                track_id = track['id']
+                artist_name = [name['name'] for name in track['artists']] 
+                songs_dict['track_id'].append(track_id)
+                songs_dict['song_name'].append(song_name)
+                songs_dict['artist_name'].append(artist_name)
+        
+        return songs_dict
